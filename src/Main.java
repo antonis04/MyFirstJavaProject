6678e5e4ed4ad493fcb59572f25a38944d4c7667
Antoni Samol <antoni.samol@gmail.com>
2025-04-07 11:58:03 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+        // This is a comment in my first java progam
+
+        /*
+            This
+            is a
+            multi-line
+            comment
+         */
+       int age = 21;
+
+       double price = 19.99;
+       double gpa = 3.5;
+       double temperature = -12.5;
+
+
+        char grade = 'A';
+        char symbol = '!';
+
+
+        boolean isStudent = true;
+        boolean forSale = false;
+
+        System.out.println(forSale);
+
+    }
+}
