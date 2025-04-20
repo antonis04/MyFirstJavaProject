5bc924474903fa3b8a5238f38c6fe24b47415d1f
Antek <antoni.samol@gmail.com>
2025-04-20 13:44:04 +0200

    Inheritance lesson in progress

--- src/Main.java
+++ src/Main.java
@@ -4,31 +4,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        // SHOPPING CART PROGRAM
 
-        Scanner scanner = new Scanner(System.in);
-
-        String item;
-        double price;
-        int quantity;
-        char currency = '$';
-        double total;
-
-        System.out.print("What item would you like to buy?: ");
-        item = scanner.nextLine();
-
-        System.out.print("What is the price for each?: ");
-        price = scanner.nextDouble();
-
-        System.out.print("How many would you like?: ");
-        quantity = scanner.nextInt();
-
-        total = price * quantity;
-
-        System.out.println("\nYou have bought " + quantity + " " + item + "/s");
-        System.out.println("The total price is " + currency + total);
+        Dog dog = new Dog();
+        Cat cat = new Cat();
 
 
-        scanner.close();
+        System.out.println(dog.isAlive);
+        System.out.println(cat.isAlive);
     }
 }
