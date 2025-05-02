5b8db76779d463fbc491942c87307ca673db43d0
Antek <antoni.samol@gmail.com>
2025-05-03 01:04:05 +0200

    First Java Project Completed

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,37 @@
+import java.time.DateTimeException;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.Scanner;
-
 
 public class Main {
     public static void main(String[] args) {
 
+        //ALARM CLOCK JAVA PROJECT
 
-        Dog dog = new Dog();
-        Cat cat = new Cat();
-        Plant plant = new Plant();
+        Scanner scanner = new Scanner(System.in);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        LocalTime alarmTime = null;
+        String filepath = "A Caring Friend.wav";
 
-        plant.photosynthesize();
+        while (alarmTime == null) {
+            try {
+                System.out.print("Enter an alarm time (HH:MM:SS): ");
+                String inputTime = scanner.nextLine();
+
+                alarmTime = LocalTime.parse(inputTime, formatter);
+                System.out.println("Alarm set for " + alarmTime);
+
+            } catch (DateTimeParseException e) {
+                System.out.println("Invalid format. Please use HH:MM:SS");
+
+            }
+        }
+            AlarmClock alarmClock = new AlarmClock(alarmTime, filepath, scanner);
+            Thread alarmThread = new Thread(alarmClock);
+            alarmThread.start();
+
+
     }
 }
+
